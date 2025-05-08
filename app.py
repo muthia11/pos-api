ecafbab02a2e575dac64f041a665b1b2a7dee427
muthia11 <147126617+muthia11@users.noreply.github.com>
2025-05-08 15:08:01 +0700

    Update app.py

--- app.py
+++ app.py
@@ -7,6 +7,15 @@ from streamlit_folium import st_folium
 
 st.set_page_config(page_title="POS BFI Terdekat", layout="centered")
 
+st.markdown("""
+    <style>
+    #MainMenu {visibility: hidden;}
+    footer {visibility: hidden;}
+    header {visibility: hidden;}
+    </style>
+""", unsafe_allow_html=True)
+
+
 # ======== FIX SCROLL DAN BACKGROUND PUTIH ==========
 st.markdown("""
     <style>
